68bf5712333203081a7686fdfaa2fd8a7509a4bc
micah <micah@DESKTOP-7BP2FJ7>
2025-07-18 18:28:28 -0500

    Add project files.

---
+++ Guess the number/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <cstdlib>
+
+int main() {
+	int number; //defining the type
+	srand(); //seeding the random function
+	printf_s("I am thinking of a number. Can you guess it?\n");
+	number = rand() % 100 + 1; // Random number between 1 and 100
+	printf_s("%d", number); //showing the number
+	return 0;
+}
