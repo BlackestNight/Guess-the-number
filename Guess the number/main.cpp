c6b5b77318b482cf7882960715422f75b37a6cbf
Micah bennett <gordenfreeman23@live.com>
2025-07-18 18:29:44 -0500

    quick before ending lunch

--- Guess the number/main.cpp
+++ Guess the number/main.cpp
@@ -6,6 +6,6 @@ int main() {
 	srand(); //seeding the random function
 	printf_s("I am thinking of a number. Can you guess it?\n");
 	number = rand() % 100 + 1; // Random number between 1 and 100
-	printf_s("%d", number); //showing the number
+	printf_s("%d", number); //showing the numberb
 	return 0;
 }
