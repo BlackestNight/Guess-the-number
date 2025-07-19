fcc75505acca5f9e48192892e7429ea5e25cd7a4
Micah bennett <gordenfreeman23@live.com>
2025-07-19 00:16:09 -0500

    some edits before bed.

--- Guess the number/main.cpp
+++ Guess the number/main.cpp
@@ -1,11 +1,26 @@
 #include <stdio.h>
 #include <cstdlib>
+#include <ctime>
+
 
 int main() {
-	int number; //defining the type
-	srand(); //seeding the random function
+	int number; //
+	int user{};
+	srand(time(NULL)); //seeding the random function
 	printf_s("I am thinking of a number. Can you guess it?\n");
 	number = rand() % 100 + 1; // Random number between 1 and 100
-	printf_s("%d", number); //showing the numberb
+	printf_s("%d", number); // for testing purposes and will be taken out later
+	printf_s("Take a guess: ");
+	scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? 
+	if (user == number) {
+		printf_s("Congrats You got it right!");
+	}
+	else if (user < number) {
+		printf_s("too low");
+	}
+	else if (user > number) {
+		printf_s("Too High");
+	}
+	//ToDo transform to a pointentail switch and case. loop it. add a exception to characters.
 	return 0;
 }
