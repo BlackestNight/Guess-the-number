068b282aafd8819413897c1d7ce50eb44af5a5dc
Micah bennett <gordenfreeman23@live.com>
2025-07-20 10:08:55 -0500

    almost there, when user inputs anything that isn't a integer to let them know.

--- Guess the number/main.cpp
+++ Guess the number/main.cpp
@@ -4,23 +4,36 @@
 
 
 int main() {
-	int number; //
+
+	int number; // varible initizlization 
 	int user{};
-	srand(time(NULL)); //seeding the random function
+	int score = 100;
+	srand(time(NULL)); //seeding the random function using the current time in seconds as the seed
 	printf_s("I am thinking of a number. Can you guess it?\n");
+	printf_s("%d Points! \n", score);
 	number = rand() % 100 + 1; // Random number between 1 and 100
-	printf_s("%d", number); // for testing purposes and will be taken out later
+	start:
 	printf_s("Take a guess: ");
-	scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? 
+	scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? w
+	while (getchar() != '\n'); // clear the buffer?
 	if (user == number) {
-		printf_s("Congrats You got it right!");
+		printf_s("Congrats You got it right! \n");
+		printf_s("%d", score);
 	}
 	else if (user < number) {
-		printf_s("too low");
+		printf_s("Too Low, you lost 10 points! \n");
+		score = score - 10;
+		goto start;
 	}
 	else if (user > number) {
-		printf_s("Too High");
+		printf_s("Too High, you lost 10 pionts \n");
+		score = score - 10;
+		goto start;
 	}
-	//ToDo transform to a pointentail switch and case. loop it. add a exception to characters.
+	else if (score == 0) {
+		printf_s("You Lose you get nothing! \n ");
+		printf_s("%d", number);
+	}
+	//ToDo add a exception to characters.
 	return 0;
 }
