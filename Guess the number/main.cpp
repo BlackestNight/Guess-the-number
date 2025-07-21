7021ba4fa1355541b6fb44a29d5ea02afcab9a10
Micah bennett <gordenfreeman23@live.com>
2025-07-21 16:20:04 -0500

    made it slightly more readable.

--- Guess the number/main.cpp
+++ Guess the number/main.cpp
@@ -2,50 +2,69 @@
 #include <cstdlib>
 #include <ctime>
 
+int random() 
+{
+	int number;	
+	srand(time(NULL)); 
+	number = rand() % 100 + 1; // Random number between 1 and 100
+	return number;
+}
 
-int main() {
+int numbers()
+{
+	int user;
+	int valid_input = 0;
+	printf_s("Take a guess: ");//take a input
+	scanf_s("%d", &user); 
+	while (getchar() != '\n'); // clear the buffer?
+	return user;
 
-	int number; // varaible initizlization 
-	int user{};
-	int score = 100;
-	double fract1{};
+}
+
+ int dynpoints(int num, int user)
+{
+	double fract1{};//local varaibles
 	double fract2{};
 	double final{};
-	srand(time(NULL)); //seeding the random function using the current time in seconds as the seed
+	fract1 = num - user;//makes the fractions for final calculations of dynamic point system. 
+	fract2 = num + user;
+	final = ((fract1 / fract2) / 2) * 100;// final calculations.
+	return round(final);//rounding the answer to a int
+}
+
+
+int main() {
+	int score = 100;
 	printf_s("I am thinking of a number. Can you guess it?\n");
 	printf_s("%d Points! \n", score);
-	while (getchar() != '\n'); // clear the buffer?
-	number = rand() % 100 + 1; // Random number between 1 and 100
-	for (int score = 100; score > 0; score - final) 
+	int num = random();
+	while (score > 0) 
 	{
-		printf_s("Take a guess: ");
-		scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? 
-		printf_s("%d points \n", score);
-		while (getchar() != '\n'); // clear the buffer?
-		fract1 = number - user;//makes the fractions for final calculations of dynamic point system. 
-		fract2 = number + user;
-		final = ((fract1 / fract2) / 2) * 100;// final calculations. 
-		if (final < 0) 
-		{
-			final = final * -1;
-		}
-		final = (int)final;//creates an int version
+		
 	
-		while (getchar() != '\n'); // clear the buffer?
-		if (user == number) 
+	int user = numbers();
+	int points = dynpoints(num, user);
+		 
+		if (points < 0)
+		{
+			points = points * -1;
+		}
+		if (user == num) 
 		{
 			printf_s("Congrats You got it right! \n");
 			printf_s("%d", score);
 			system("pause");
 			return 0;
 		}
-		else if (user < number) 
+		else if (user < num) 
 		{
-			printf_s("Too Low, you lost %d points! \n", (int)final);
+			printf_s("Too Low, you lost %d points! \n", points);
+			score -= points;
 		}
-		else if (user > number) 
+		else if (user > num) 
 		{
-			printf_s("Too High, you lost %d pionts \n", (int)final);
+			printf_s("Too High, you lost %d points \n", points);
+			score -= points;
 		}
 	}
 	if (score <= 0) 
@@ -55,3 +74,4 @@ int main() {
 	//ToDo add a exception to characters, and make it look better, seperate functions to make it look better and cleaner. 
 	return 0;
 }
+
