4c53e71771412534899fd335ad34faaf9ad04e04
Micah bennett <gordenfreeman23@live.com>
2025-07-21 13:19:13 -0500

    this looks like shit but is now a for loop that makes lets you play the game untill the score is reached.

--- Guess the number/main.cpp
+++ Guess the number/main.cpp
@@ -15,35 +15,37 @@ int main() {
 	printf_s("I am thinking of a number. Can you guess it?\n");
 	printf_s("%d Points! \n", score);
 	number = rand() % 100 + 1; // Random number between 1 and 100
-	start:
-	printf_s("Take a guess: ");
-	scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? 
-	printf_s("%d points \n", score);
-	fract1 = number - user;
-	fract2 = number + user;
-	final = ((fract1 / fract2) / 2) * 100;// still need to work out negative numbers? maybe use an unsigned data type
-	if (final < 0) {
-		final = final * -1;
-	}
-	final = (int)final;//creates an int version
-	while (getchar() != '\n'); // clear the buffer?
-	while (score != 0 && score > 0) {
+	for (int score = 100; score > 0; score - final) {
+		printf_s("Take a guess: ");
+		scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? 
+		printf_s("%d points \n", score);
+		fract1 = number - user;//makes the fractions for final calculations of dynamic point system. 
+		fract2 = number + user;
+		final = ((fract1 / fract2) / 2) * 100;// final calculations. 
+		if (final < 0) {
+			final = final * -1;
+		}
+		final = (int)final;//creates an int version
+	
+		while (getchar() != '\n'); // clear the buffer?
 		if (user == number) {
 			printf_s("Congrats You got it right! \n");
 			printf_s("%d", score);
 			system("pause");
+			return 0;
 		}
 		else if (user < number) {
 			printf_s("Too Low, you lost %d points! \n", (int)final);
 			score = score - final;
-			goto start;
 		}
 		else if (user > number) {
 			printf_s("Too High, you lost %d pionts \n", (int)final);
 			score = score - final;
-			goto start;
 		}
 	}
-	//ToDo add a exception to characters.
+	if (score <= 0) {
+		printf_s("Game OVER!");
+	}
+	//ToDo add a exception to characters, and make it look better, seperate functions to make it look better and cleaner. 
 	return 0;
 }
