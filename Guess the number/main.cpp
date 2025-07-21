9e12ae7deed3ac6866a27b2ae0b7a6f1ef5f82e2
Micah bennett <gordenfreeman23@live.com>
2025-07-20 23:41:28 -0500

    fixed the negative numbers situation before bed

--- Guess the number/main.cpp
+++ Guess the number/main.cpp
@@ -17,31 +17,32 @@ int main() {
 	number = rand() % 100 + 1; // Random number between 1 and 100
 	start:
 	printf_s("Take a guess: ");
-	scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? w
+	scanf_s("%d", &user);//your user input will need to be clear with each loop? should i be using a for loop for this? 
+	printf_s("%d points \n", score);
 	fract1 = number - user;
 	fract2 = number + user;
-	final = ((fract1 / fract2) / 2) * 10;// still need to work out negative numbers? maybe use an unsigned data type
+	final = ((fract1 / fract2) / 2) * 100;// still need to work out negative numbers? maybe use an unsigned data type
+	if (final < 0) {
+		final = final * -1;
+	}
 	final = (int)final;//creates an int version
 	while (getchar() != '\n'); // clear the buffer?
-	if (user == number) {
-		printf_s("Congrats You got it right! \n");
-		printf_s("%d", score);
-		system("pause");
-	}
-	else if (user < number) {
-		printf_s("Too Low, you lost %d points! \n", (int)final);
-		score = score - final;
-		goto start;
-	}
-	else if (user > number) {
-		printf_s("Too High, you lost %d pionts \n", (int)final);
-		score = score - final;
-		goto start;
-	}
-	else if (score == 0) {
-		printf_s("You Lose you get nothing! \n ");
-		printf_s("%d", number);
-		system("pause");
+	while (score != 0 && score > 0) {
+		if (user == number) {
+			printf_s("Congrats You got it right! \n");
+			printf_s("%d", score);
+			system("pause");
+		}
+		else if (user < number) {
+			printf_s("Too Low, you lost %d points! \n", (int)final);
+			score = score - final;
+			goto start;
+		}
+		else if (user > number) {
+			printf_s("Too High, you lost %d pionts \n", (int)final);
+			score = score - final;
+			goto start;
+		}
 	}
 	//ToDo add a exception to characters.
 	return 0;
